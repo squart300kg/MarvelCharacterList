c0b3c306088065244e678f4931a5e735993634bd
n22301003-sysong <sysong@pentasecurity.com>
2023-10-08 03:16:30 +0900

    앱 첫 로딩시, bottomBar selected 설정 작업

--- app/src/main/java/kr/co/korean/investment/MainActivity.kt
+++ app/src/main/java/kr/co/korean/investment/MainActivity.kt
@@ -3,20 +3,29 @@ package kr.co.korean.investment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import kr.co.korean.investment.ui.navigation.TopLevelDestination
 import kr.co.korean.investment.ui.theme.KoreanInvestmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +34,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val navController = rememberNavController()
+
             KoreanInvestmentTheme {
 
                 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,56 +43,71 @@ class MainActivity : ComponentActivity() {
                     snackbarHost = {},
                     bottomBar = {
                         NavigationBar {
-                            NavigationBarItem(
-                                selected = true,
-                                onClick = {},
-                                icon = {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_home_filled),
-                                        contentDescription = null
-                                    )
-                                }
-                            )
-                            NavigationBarItem(
-                                selected = true,
-                                onClick = {},
-                                icon = {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_bookmark_filled),
-                                        contentDescription = null
-                                    )
-                                }
-                            )
+                            TopLevelDestination.values().asList().forEach { destination ->
+                                val selected = navController
+                                    .getCurrentDestination()
+                                    .isTopLevelDestinationInHierarchy(destination)
+
+                                NavigationBarItem(
+                                    selected = selected,
+                                    onClick = {},
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(id = if (selected) {
+                                                destination.selectedIconRes
+                                            } else {
+                                                destination.unselectedIconRes
+                                            }),
+                                            contentDescription = null
+                                        )
+                                    },
+                                    label = { Text(stringResource(id = destination.iconTextIdRes)) }
+                                )
+                            }
                         }
                     }
                 ) { innerPadding ->
-                    Text(
-                        modifier = Modifier.padding(innerPadding),
-                        text = "sdf")
+                    NavHost(
+                        navController = navController,
+                        startDestination = TopLevelDestination.HOME.name,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable(route = TopLevelDestination.HOME.name) {
+                            HomeScreen()
+                        }
+                        composable(route = TopLevelDestination.BOOKMARKS.name) {
+                            BookmarkScreen()
+                        }
+
+                    }
                 }
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.background(Color.Cyan).fillMaxSize())
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    KoreanInvestmentTheme {
-        Greeting("Android")
-    }
+fun BookmarkScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.background(Color.Blue).fillMaxSize())
 }
+
+@Composable
+private fun NavHostController.getCurrentDestination(): NavDestination? {
+    return this.currentBackStackEntryAsState()
+            .value
+            ?.destination
+}
+
+private fun NavDestination?.isTopLevelDestinationInHierarchy(destination: TopLevelDestination) =
+    this?.hierarchy?.any {
+        it.route?.contains(destination.name, true) ?: false
+    } ?: false
---
+++ app/src/main/java/kr/co/korean/investment/ui/navigation/TopLevelDestination.kt
@@ -0,0 +1,22 @@
+package kr.co.korean.investment.ui.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import kr.co.korean.investment.R
+
+enum class TopLevelDestination(
+    @DrawableRes val selectedIconRes: Int,
+    @DrawableRes val unselectedIconRes: Int,
+    @StringRes val iconTextIdRes: Int,
+) {
+    HOME(
+        selectedIconRes = R.drawable.ic_tab_home_filled,
+        unselectedIconRes = R.drawable.ic_tab_home,
+        iconTextIdRes = R.string.tabHome,
+    ),
+    BOOKMARKS(
+        selectedIconRes = R.drawable.ic_tab_bookmark_filled,
+        unselectedIconRes = R.drawable.ic_tab_bookmark,
+        iconTextIdRes = R.string.tabBookMark,
+    ),
+}
