78204e06bd5812d56e30f385e1398e33ff1e81e3
n22301003-sysong <sysong@pentasecurity.com>
2023-10-08 01:21:49 +0900

    하단 탭바 벡터 아이콘 추가

--- app/src/main/java/kr/co/korean/investment/MainActivity.kt
+++ app/src/main/java/kr/co/korean/investment/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -13,34 +15,56 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import kr.co.korean.investment.ui.theme.KoreanInvestmentTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             KoreanInvestmentTheme {
-//                Scaffold(
-//                    bottomBar = NavigationBar {
-//                        NavigationBarItem(
-//                            selected = true,
-//                            onClick = {},
-//                            icon = {}
-//                        )
-//                    }
-//                ) { innerPadding ->
-//                    Text(
-//                        modifier = Modifier.padding(innerPadding),
-//                        text = "sdf")
-//                }
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+                @OptIn(ExperimentalMaterial3Api::class)
+                Scaffold(
+                    snackbarHost = {},
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                selected = true,
+                                onClick = {},
+                                icon = {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_home_filled),
+                                        contentDescription = null
+                                    )
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = true,
+                                onClick = {},
+                                icon = {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_bookmark_filled),
+                                        contentDescription = null
+                                    )
+                                }
+                            )
+                        }
+                    }
+                ) { innerPadding ->
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "sdf")
                 }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
             }
         }
     }
