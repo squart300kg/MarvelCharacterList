f25d471c94e8c5b9458c02bcf79eeecc0dcc6a82
n22301003-sysong <sysong@pentasecurity.com>
2023-10-09 23:59:17 +0900

    CharactersResponse 작성

---
+++ data/network/src/main/java/kr/co/korean/network/model/CharactersResponse.kt
@@ -0,0 +1,93 @@
+package kr.co.korean.network.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CharactersResponse(
+    val attributionHTML: String,
+    val attributionText: String,
+    val code: Int,
+    val copyright: String,
+    val data: Data,
+    val etag: String,
+    val status: String,
+) {
+    data class Data(
+        val count: Int,
+        val limit: Int,
+        val offset: Int,
+        val results: List<Result>,
+        val total: Int
+    ) {
+        data class Result(
+            val comics: Comics,
+            val description: String,
+            val events: Events,
+            val id: Int,
+            val modified: String,
+            val name: String,
+            val resourceURI: String,
+            val series: Series,
+            val stories: Stories,
+            val thumbnail: Thumbnail,
+            val urls: List<Url>
+        ) {
+
+            sealed interface Item {
+
+                val name: String
+                val resourceURI: String
+                data class NormalItem(
+                    override val name: String,
+                    override val resourceURI: String
+                ): Item
+
+                data class StoryItem(
+                    override val name: String,
+                    override val resourceURI: String,
+                    val type: String
+                ): Item
+            }
+
+
+            data class Comics(
+                val available: Int,
+                val collectionURI: String,
+                val items: List<Item.NormalItem>,
+                val returned: Int
+            )
+
+            data class Events(
+                val available: Int,
+                val collectionURI: String,
+                val items: List<Item.NormalItem>,
+                val returned: Int
+            )
+
+            data class Series(
+                val available: Int,
+                val collectionURI: String,
+                val items: List<Item.NormalItem>,
+                val returned: Int
+            )
+
+            data class Stories(
+                val available: Int,
+                val collectionURI: String,
+                val items: List<Item.StoryItem>,
+                val returned: Int
+            )
+
+            data class Thumbnail(
+                val extension: String,
+                val path: String
+            )
+
+            data class Url(
+                val type: String,
+                val url: String
+            )
+
+        }
+    }
+}
