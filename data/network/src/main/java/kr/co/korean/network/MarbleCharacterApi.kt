c2f27ac61b06275f94fc1b1e7b337088e2b325fd
n22301003-sysong <sysong@pentasecurity.com>
2023-10-10 00:01:31 +0900

    retrofit인터페이스 작성 (/v1/public/characters)

--- data/network/src/main/java/kr/co/korean/network/MarbleCharacterApi.kt
+++ data/network/src/main/java/kr/co/korean/network/MarbleCharacterApi.kt
@@ -1,5 +1,15 @@
 package kr.co.korean.network
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MarbleCharacterApi {
+
+    @GET("/v1/public/characters")
+    fun getCharacters(
+        @Query("apikey") apiKey: String,
+        @Query("ts") timeStamp: Long,
+        @Query("hash") hash: String,
+    )
 
 }
