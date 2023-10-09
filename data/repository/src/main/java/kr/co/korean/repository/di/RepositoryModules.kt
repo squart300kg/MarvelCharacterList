ae211be87f6bb5d9a86f06d731b81a6e2d328aeb
n22301003-sysong <sysong@pentasecurity.com>
2023-10-10 00:07:36 +0900

    RepositoryModules 작성

---
+++ data/repository/src/main/java/kr/co/korean/repository/di/RepositoryModules.kt
@@ -0,0 +1,11 @@
+package kr.co.korean.repository.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModules {
+
+}
