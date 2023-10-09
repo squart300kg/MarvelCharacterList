1e5eb903cab341488619720ed48fc933d575e685
n22301003-sysong <sysong@pentasecurity.com>
2023-10-09 18:10:09 +0900

    DevicePreviews추가

--- ui/src/main/java/kr/co/korean/ui/home/HomeScreen.kt
+++ ui/src/main/java/kr/co/korean/ui/home/HomeScreen.kt
@@ -1,13 +1,15 @@
 package kr.co.korean.ui.home
 
-import android.widget.TextView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +18,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -33,18 +38,27 @@ fun HomeScreen(
     LazyColumn(modifier = modifier) {
         items(characterUiState) { uiModel ->
             Box(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(dimensionResource(id = R.dimen.characterItemCommonPadding))
             ) {
                 Row(
                     modifier = Modifier.align(Alignment.CenterStart)
                 ) {
                     Image(
-                        modifier = Modifier.size(90.dp),
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically),
                         painter = rememberAsyncImagePainter(uiModel.thumbnail),
                         contentDescription = null
                     )
 
-                    Column {
+                    Column(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.primary)
+                            .fillMaxHeight()
+                        , verticalArrangement = Arrangement.SpaceBetween
+                    ) {
                         Text(
                             modifier = modifier,
                             text = stringResource(id = R.string.characterItemUrlCount) + uiModel.urlCount.toString())
@@ -62,20 +76,15 @@ fun HomeScreen(
                             text = stringResource(id = R.string.characterItemSeriesCount) + uiModel.seriesCount.toString())
                     }
                 }
+
+                Image(
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(dimensionResource(id = R.dimen.characterItemCommonPadding)),
+                    painter = painterResource(id = R.drawable.ic_bookmark_select),
+                    contentDescription = null)
             }
         }
 
     }
-}
-
-@Composable
-fun CharacterData(
-    modifier: Modifier = Modifier,
-    data: String,
-) {
-    Text(
-        modifier = modifier,
-        text = data,
-
-    )
 }
