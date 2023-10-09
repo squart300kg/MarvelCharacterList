f7a407898c5258dd7fe0543334c64d2c65732473
n22301003-sysong <sysong@pentasecurity.com>
2023-10-10 00:03:41 +0900

    md5 메서드 common패키지로 이동

---
+++ common/src/main/java/kr/co/korean/common/MD5Generator.kt
@@ -0,0 +1,9 @@
+package kr.co.korean.common
+
+import java.math.BigInteger
+import java.security.MessageDigest
+
+fun encodeToMd5(input:String): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+}
