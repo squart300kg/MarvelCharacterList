fbc28485fb2338442e1aba297189c3c188f88e3c
n22301003-sysong <sysong@pentasecurity.com>
2023-10-10 12:54:54 +0900

    datastore모듈 내, datastore의존성 추가

--- data/datastore/build.gradle.kts
+++ data/datastore/build.gradle.kts
@@ -44,10 +44,9 @@ dependencies {
 
     implementation(libs.com.google.dagger.hilt.android)
     kapt(libs.com.google.dagger.hilt.compiler)
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.dataStore.core)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
 }
