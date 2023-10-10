1b7b40261c37bc7357fffa749f5d29ddcbb443f7
n22301003-sysong <sysong@pentasecurity.com>
2023-10-10 11:16:12 +0900

    RepositoryModules내, 레파지토리 추가

--- data/repository/src/main/java/kr/co/korean/repository/CharacterRepositoryImpl.kt
+++ data/repository/src/main/java/kr/co/korean/repository/CharacterRepositoryImpl.kt
@@ -39,5 +39,4 @@ class CharacterRepositoryImpl @Inject constructor(
             }
         }
     }
-
 }
--- data/repository/src/main/java/kr/co/korean/repository/di/RepositoryModules.kt
+++ data/repository/src/main/java/kr/co/korean/repository/di/RepositoryModules.kt
@@ -1,11 +1,19 @@
 package kr.co.korean.repository.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kr.co.korean.network.MarvelCharacterApi
+import kr.co.korean.repository.CharacterRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface RepositoryModules {
+
+    @Binds
+    fun bindsCharacterRepository(
+        marvelCharacterApi: MarvelCharacterApi
+    ): CharacterRepository
 
 }
