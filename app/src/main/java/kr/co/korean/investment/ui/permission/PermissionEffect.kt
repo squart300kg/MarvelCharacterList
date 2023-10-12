9841663bf2a1bb928fb98a573e63034e0fa17490
n22301003-sysong <sysong@pentasecurity.com>
2023-10-13 00:48:17 +0900

    주석 지움

--- app/src/main/java/kr/co/korean/investment/ui/permission/PermissionEffect.kt
+++ app/src/main/java/kr/co/korean/investment/ui/permission/PermissionEffect.kt
@@ -1,12 +1,10 @@
 package kr.co.korean.investment.ui.permission
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.shouldShowRationale
-import kr.co.korean.common.model.Result
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -21,30 +19,20 @@ fun PermissionEffect(
     multiplePermissionsState.permissions.forEach { permissionState ->
         LaunchedEffect(permissionState.status) {
             if (multiplePermissionsState.allPermissionsGranted) {
-//                multiplePermissionsState = true
                 onPermissionStateChanged(true)
             } else {
 
                 // 앱 첫 진입 및 권한 다이얼로그 로딩
-                Log.e("permission", "appFirstStartedState : $multiplePermissionsState")
                 if (!appFirstStartedState) {
-                    Log.e("permission", "first")
                     onShowFirst()
-//                    viewModel.startApp()
-//                    requiredPermissionsStates.launchMultiplePermissionRequest()
                 } else {
                     // 권한 거절 클릭
                     if (permissionState.status.shouldShowRationale) {
                         // 첫 번째 권한 거절로, 권한 필요 다이얼로그를 로딩하며, 권한 재요청
                         onUserNagativeClickFirst()
-//                        requiredPermissionsStates.launchMultiplePermissionRequest()
-//                        viewModel.showPermissionNeededDialog()
-                        Log.e("permission", "shouldShowRationale true")
                     } else {
                         onUserNagativeClickSecond()
                         // 두 번째 권한 거절로, 권한 설정 페이지 리다이렉트 다이얼로그 로딩
-//                        viewModel.showPermissionSettingRedirectDialog()
-                        Log.e("permission", "shouldShowRationale false")
                     }
                 }
             }
