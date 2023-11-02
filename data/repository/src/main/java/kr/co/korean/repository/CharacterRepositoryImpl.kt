8516bc4d98f5882699e02c862a27a74e83d69ad6
n22301003-sysong <sysong@pentasecurity.com>
2023-11-02 17:07:04 +0900

    레파지토리 내, convert로직 수정

--- data/repository/src/main/java/kr/co/korean/repository/CharacterRepositoryImpl.kt
+++ data/repository/src/main/java/kr/co/korean/repository/CharacterRepositoryImpl.kt
@@ -14,6 +14,7 @@ import kr.co.korean.network.BuildConfig
 import kr.co.korean.network.CHARACTER_DATA_PAGE_SIZE
 import kr.co.korean.network.MarvelCharacterApi
 import kr.co.korean.network.MarvelCharacterPagingSource
+import kr.co.korean.network.model.CharactersResponseModel
 import kr.co.korean.repository.model.CharacterDataModel
 import kr.co.korean.work.ImageDownLoadResult
 import kr.co.korean.work.ThumbnailDownloadDataSource
@@ -30,17 +31,31 @@ fun CharacterDataModel.convertRoomModel() =
         eventCount = eventCount
     )
 
-fun convertDataModel(roomModel: MarvelCharacter) =
-    CharacterDataModel(
-        id = roomModel.characterId,
-        thumbnail = roomModel.thumbnail,
-        urlCount = roomModel.urlCount,
-        comicCount = roomModel.comicCount,
-        storyCount = roomModel.storyCount,
-        seriesCount = roomModel.seriesCount,
-        eventCount = roomModel.eventCount
-    )
+fun convertDataModel(roomModels: List<MarvelCharacter>) =
+    roomModels.map { marvelCharacter ->
+        CharacterDataModel(
+            id = marvelCharacter.characterId,
+            thumbnail = marvelCharacter.thumbnail,
+            urlCount = marvelCharacter.urlCount,
+            comicCount = marvelCharacter.comicCount,
+            storyCount = marvelCharacter.storyCount,
+            seriesCount = marvelCharacter.seriesCount,
+            eventCount = marvelCharacter.eventCount
+        )
+    }
 
+fun convertDataModel(pagingData: PagingData<CharactersResponseModel.Data.Result>) =
+    pagingData.map { pagingData ->
+        CharacterDataModel(
+            id = pagingData.id,
+            thumbnail = pagingData.thumbnail.imageFullPath,
+            urlCount = pagingData.urls.size,
+            comicCount = pagingData.comics.returned,
+            seriesCount = pagingData.series.returned,
+            storyCount = pagingData.stories.returned,
+            eventCount = pagingData.events.returned
+        )
+    }
 /**
  * Repository의 경우, API의 'XXXResponse' 네이밍의 모델 수신 후,
  * 상위 레이어로 전달을 위한 모델('XXXDataModel')로 파싱 및 Ui Layer or Data Layer에 노출합니다.
@@ -52,27 +67,14 @@ class CharacterRepositoryImpl @Inject constructor(
 ): CharacterRepository {
 
     override val localCharacters: Flow<List<CharacterDataModel>> =
-        marvelCharacterDao.getAllCharacter().map {
-            it.map(::convertDataModel)
-        }
+        marvelCharacterDao.getAllCharacter()
+            .map(::convertDataModel)
 
     override val remoteCharacters: Flow<PagingData<CharacterDataModel>> =
         Pager(
             config = PagingConfig(pageSize = CHARACTER_DATA_PAGE_SIZE),
             pagingSourceFactory = { MarvelCharacterPagingSource(marvelCharacterApi) }
-        ).flow.map { pagingDatas ->
-            pagingDatas.map { pagingData ->
-                CharacterDataModel(
-                    id = pagingData.id,
-                    thumbnail = pagingData.thumbnail.imageFullPath,
-                    urlCount = pagingData.urls.size,
-                    comicCount = pagingData.comics.returned,
-                    seriesCount = pagingData.series.returned,
-                    storyCount = pagingData.stories.returned,
-                    eventCount = pagingData.events.returned
-                )
-            }
-        }
+        ).flow.map(::convertDataModel)
 
     override suspend fun modifyCharacterSavedStatus(dataModel: CharacterDataModel, saved: Boolean) {
         val roomModel = dataModel.convertRoomModel()
