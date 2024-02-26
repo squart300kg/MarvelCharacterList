2147d8b7b52a00b2dc9ebb5ef334657c79212f18
n22301003-sysong <sysong@pentasecurity.com>
2024-02-26 15:42:39 +0900

    CharacterDataModel확장 시도

--- data/repository/src/main/java/kr/co/korean/repository/model/CharacterDataModel.kt
+++ data/repository/src/main/java/kr/co/korean/repository/model/CharacterDataModel.kt
@@ -3,6 +3,8 @@ package kr.co.korean.repository.model
 data class CharacterDataModel(
     val id: Int,
     val thumbnail: String,
+    val name: String,
+    val description: String,
     val urlCount: Int,
     val comicCount: Int,
     val storyCount: Int,
