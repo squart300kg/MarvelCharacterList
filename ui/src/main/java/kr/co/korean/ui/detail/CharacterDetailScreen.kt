17103217d24122647e3af7777db1d2e8b1548f05
n22301003-sysong <sysong@pentasecurity.com>
2024-02-27 14:53:42 +0900

    CharacterDetailScreen추가

---
+++ ui/src/main/java/kr/co/korean/ui/detail/CharacterDetailScreen.kt
@@ -0,0 +1,11 @@
+package kr.co.korean.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CharacterDetailScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
