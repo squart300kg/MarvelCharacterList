a1aee40a3c4887c72c60ffd1a3816074341990f1
n22301003-sysong <sysong@pentasecurity.com>
2024-03-26 19:39:25 +0900

    ResponseModels내, SerializedName 어너테이션 추가

--- data/network/src/main/java/kr/co/korean/network/model/common/ResponseModel.kt
+++ data/network/src/main/java/kr/co/korean/network/model/common/ResponseModel.kt
@@ -1,19 +1,33 @@
 package kr.co.korean.network.model.common
 
+import com.google.gson.annotations.SerializedName
+
 data class ResponseModel(
+    @SerializedName("attributionHTML")
     val attributionHTML: String,
+    @SerializedName("attributionText")
     val attributionText: String,
+    @SerializedName("code")
     val code: Int,
+    @SerializedName("copyright")
     val copyright: String,
+    @SerializedName("data")
     val data: Data,
+    @SerializedName("etag")
     val etag: String,
+    @SerializedName("status")
     val status: String
 ) {
     data class Data(
+        @SerializedName("count")
         val count: Int,
+        @SerializedName("limit")
         val limit: Int,
+        @SerializedName("offset")
         val offset: Int,
+        @SerializedName("results")
         val results: List<Result>,
+        @SerializedName("total")
         val total: Int
     )
 }
