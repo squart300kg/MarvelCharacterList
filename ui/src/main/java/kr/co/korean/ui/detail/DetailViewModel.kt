e236e72ce6f792fd61b448fb16c43cce210b41b5
n22301003-sysong <sysong@pentasecurity.com>
2024-03-27 17:54:56 +0900

    DetailViewModel보완

--- ui/src/main/java/kr/co/korean/ui/detail/DetailViewModel.kt
+++ ui/src/main/java/kr/co/korean/ui/detail/DetailViewModel.kt
@@ -5,26 +5,35 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kr.co.korean.common.model.UiResult
 import kr.co.korean.model.convertUiModel
 import kr.co.korean.repository.CharacterRepository
-import kr.co.korean.repository.ContentsType
-import kr.co.korean.ui.detail.navigation.DETAIL_CONTENTS_TYPE_ARG
 import kr.co.korean.ui.detail.navigation.DETAIL_ID_ARG
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val characterRepository: CharacterRepository
 ): ViewModel() {
 
-
+    private val id: StateFlow<String?> = savedStateHandle.getStateFlow(DETAIL_ID_ARG, null)
+    val uiState = id.filterNotNull()
+        .flatMapLatest { characterRepository.getRemoteSingleCharacter(it.toInt()) }
+        .catch { Log.e("detailErrorLog", it.stackTraceToString()) }
+        .map { UiResult.Success(it.map(::convertUiModel)) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = UiResult.Loading
+        )
 }
