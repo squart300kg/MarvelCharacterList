48401c3cb087824e4a4065879d80e55da0084042
n22301003-sysong <sysong@pentasecurity.com>
2024-03-28 13:57:59 +0900

    BaseNavigationRailBarItem추가

---
+++ app/src/main/java/kr/co/korean/investment/ui/navigation/BaseNavigationRailBarItem.kt
@@ -0,0 +1,33 @@
+package kr.co.korean.investment.ui.navigation
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+
+@Composable
+internal fun BaseNavigationRailBarItem(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    selected: Boolean,
+    destination: BaseDestination
+) {
+    NavigationRailItem(
+        modifier = modifier,
+        selected = selected,
+        onClick = onClick,
+        icon = {
+            Icon(
+                painter = painterResource(id = if (selected) {
+                    destination.selectedIconRes
+                } else {
+                    destination.unselectedIconRes
+                }),
+                contentDescription = null
+            )
+        },
+        label = { Text(stringResource(id = destination.iconTextIdRes)) })
+}
