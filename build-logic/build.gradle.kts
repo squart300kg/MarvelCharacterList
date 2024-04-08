e26bc6f92ae222588f6d14b69e84501961771951
n22301003-sysong <sysong@pentasecurity.com>
2024-04-08 23:30:35 +0900

    build-logic내부 개발

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,39 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin)
+    `kotlin-dsl`
 }
 
-android {
-    namespace = "kr.co.architecture.build-logic"
-    compileSdk = 33
+group = "kr.co.architecture.build.logic"
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner =
-            "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+// Configure the build-logic plugins to target JDK 17
+// This matches the JDK used to build the project, and is not related to what is running on device.
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+}
 
-    dependencies {
+dependencies {
 
-        testImplementation(libs.junit)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "nowinandroid.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
     }
 }
---
+++ build-logic/src/main/java/kr/co/architecture/build/logic/plugin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,14 @@
+package kr.co.architecture.build.logic.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidApplicationConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+
+            }
+        }
+    }
+}
